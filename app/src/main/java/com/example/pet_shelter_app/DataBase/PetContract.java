fdbc4c90c659b109cc2c304f2b58b66be7630767
Bhagwat singh <48333371+bhagwat8024@users.noreply.github.com>
2021-06-17 15:43:26 +0530

    Update PetContract.java

--- app/src/main/java/com/example/pet_shelter_app/DataBase/PetContract.java
+++ app/src/main/java/com/example/pet_shelter_app/DataBase/PetContract.java
@@ -30,14 +30,6 @@ public class PetContract {
             return false;
         }
 
-        public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
-
-        /**
-         * The MIME type of the {@link #CONTENT_URI} for a single pet.
-         */
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
     }
 
 }
